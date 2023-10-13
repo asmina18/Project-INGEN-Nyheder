5d46d24b74b34abd3217be87da2a82f462bf536e
Amina <113165708+asmina18@users.noreply.github.com>
2023-10-13 13:18:52 +0200

    Update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,16 +5,14 @@ export const Footer = () => {
     return (
         <footer className={style.footerContainer}>
             {/* /--------------------------------------------------------------------------/ */}
-            <div className={style.adressStyle}>
+            <div>
                 <ul>
                     <li>Adress:</li><br />
                     <li>Intet nyt - Godt nyt ApS</li><br />
                     <li>Tulipanvej 232</li>
                     <li>7320 <br />Valby Øster</li>
                 </ul>
-            </div>
-            {/* /--------------------------------------------------------------------------/ */}
-            <div className={style.linksStyle}>
+                {/* /--------------------------------------------------------------------------/ */}
                 <ul>
                     <li>Links</li><br />
                     <li>vikanweb.dkl</li>
@@ -22,9 +20,7 @@ export const Footer = () => {
                     <li>  retsinformation.dk</li>
                     <li> nogetmednews.dk </li>
                 </ul>
-            </div>
-            {/* /--------------------------------------------------------------------------/ */}
-            <div className={style.politkStyle}>
+                {/* /--------------------------------------------------------------------------/ */}
                 <ul>
                     <li>Politik</li><br />
                     <li>Privatlivspolitik</li>
@@ -32,9 +28,7 @@ export const Footer = () => {
                     <li>Købsinformation</li>
                     <li>Delingspolitik</li>
                 </ul>
-            </div>
-            {/* /--------------------------------------------------------------------------/ */}
-            <div className={style.kontaktStyle}>
+                {/* /--------------------------------------------------------------------------/ */}
                 <ul>
                     <li>Kontakt</li><br />
                     <li><a href="#">ingen@nyhed.dk</a></li>
